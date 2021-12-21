893e72eb92069621091a8b1a70823b4831c1bdd2
Austin Amento <puggsy31@aol.com>
2021-12-20 19:32:05 -0800

    start on coffee brew app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
+// import {data} from './cardData.json';
+import LogoSrc from './assets/images/Chemex.png';
+
+// console.log(data)
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;  
+    font-family: monospace, sans-serif;
+    background-color: #404040;
+    text-align: center;
+  }
+`; 
+
+const Title = styled.h1`
+  color: #ffffff;
+  text-align: center;
+`;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Card = styled.div`
+  background-color: #b3b3b3;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-space-evenly;
+  align-items: center;
+  width: 200px;
+  height: 300px;
+  border-radius: 5px;
+  border: 3px solid #181818;
+`;
+const Subtitle = styled.h2`
+  color: #ffffff;
+  border-bottom: 3px solid #181818;
+  width: 100%;
+`;
+const Logo = styled.img`
+  width: 100px;
+  height: 150px;
+`;
+const Button = styled.button`
+  background-color: #181818;
+  color: #ffffff;
+  font-size: 15px;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  margin-top: 15px;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Title>Coffee Brew Cards</Title>
+      <Container>
+        <Card>
+          <Subtitle>Chemex</Subtitle>
+          <Logo src={LogoSrc} />
+          <Button>Brew</Button>
+        </Card>
+      </Container>
+
+    </>
+    
   );
 }
 
