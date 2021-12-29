c96976081107bb68c2f08ebd29c4c0a00e4f7a2d
Austin Amento <puggsy31@aol.com>
2021-12-28 16:25:42 -0800

    rendered each card

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { useState } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import styled from 'styled-components';
-// import {data} from './cardData.json';
-import LogoSrc from './assets/images/Chemex.png';
-import Timer from './components/Timer';
+import NewCard from './components/Card';
+import BackCardComponent from './components/BackCard';
 
 
+const data = require('./cardData.json');
 
+console.log( typeof data);
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;  
@@ -21,97 +22,13 @@ const Title = styled.h1`
   text-align: center;
 `;
 const Container = styled.div`
-  display: flex;
+  display: grid;
+  grid: 1fr 1fr / 1fr 1fr 1fr;
+  gap: 20px;
   justify-content: center;
   align-items: center;
 `;
 
-const Card = styled.div`
-  background-color: #b3b3b3;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-  width: 300px;
-  height: 400px;
-  border-radius: 5px;
-  border: 3px solid #181818;
-  box-shadow: 0px 0px 15px 3px #b3b3b3;
-  transition: transform 0.8s;
-  transform-style: preserve-3d;
-
-
-`;
-const Subtitle = styled.h2`
-  color: #ffffff;
-  font-size: 2rem;
-  text-align: center;
-`;
-const Logo = styled.img`
-  width: 100px;
-  height: 150px;
-  margin-bottom: 5px;
-`;
-
-const RecipeHeader = styled.h5`
-  color: #181818;
-  text-align: center;
-  margin: 0;
-`;
-
-
-const Recipe = styled.p`
-  color: #ffffff;
-  font-size: 12px;
-  margin: 0 0 75px;
-  padding: 5px 0;
-  text-align: center;
-  background-color: #181818;
-`;
-
-
-const Button = styled.button`
-  background-color:${({primary}) => primary ? "#181818" : "#b3b3b3"}; 
-  position: absolute;
-  color: #ffffff;
-  font-size: 15px;
-  bottom: 10px;
-  border: none;
-  border-radius: 5px;
-  padding: 10px 20px;
-  margin-top: 15px;
-`;
-const BackCard = styled.div`
-  background-color: #181818;
-  color: #ffffff;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 300px;
-  height: 400px;
-  border-radius: 5px;
-  border: 3px solid #b3b3b3;
-  box-shadow: 0px 0px 15px 5px #181818;
-
-  transition: transform 0.8s;
-  transform-style: preserve-3d;
-`;
-const BrewInstructions = styled.ol`
-  color: #ffffff;
-`;
-const BrewStep = styled.li`
-  color: #ffffff; 
-  font-size: 11px;
-  line-height: 1.5;
-  `;
-const Quote = styled.p`
-  color: #181818;
-  text-align: center;
-  background-color: #b3b3b3;
-  font-size: 10px;
-  padding: 10px 5px;
-  margin: 0;
-`;
 
 function App() {
 
@@ -124,34 +41,12 @@ function App() {
       <GlobalStyle />
       <Title>Coffee Brew Cards</Title>
       <Container>
-{      flip ?  
-        <Card>
-          <Subtitle>Chemex</Subtitle>
-          <Logo src={LogoSrc} />
-          <RecipeHeader>Things Needed:</RecipeHeader>
-          <Recipe>660g Water / 44g Coffee / Chemex / Chemex Filters / Gooseneck Kettle / Scale / Timer</Recipe>
-          <Button primary onClick={handleClick}>Brew</Button>
-        </Card>
-        : <BackCard>
-          <Subtitle>Chemex Recipe</Subtitle>
-          <Quote>"The Chemex is as aesthetically pleasing as it is able to make delicious, well-rounded coffee for you and your friend(s)." -Augie</Quote>
-          <BrewInstructions>
-            <BrewStep>Heat Water to 205 F</BrewStep>
-            <BrewStep>Place filter in Chemex, and rinse with hot water</BrewStep>
-            <BrewStep>Coarse grind coffee and add to Chemex</BrewStep>
-            <BrewStep>Add 75g of water and let bloom for 45seconds</BrewStep>
-            <BrewStep>Continue adding water in Heavy 75g pours until you reached your total water amount</BrewStep>
-            <BrewStep>Total brew time should be 3:30-4:30</BrewStep>
-            <BrewStep>Wait for 30 seconds</BrewStep>
-          </BrewInstructions>
-          <Container>
-            <Timer />
-            <Button onClick={handleClick}>Back</Button>
-          </Container>
-          
-        </BackCard> }
+        {data && data.map((recipe, index) => {
+          return (
+            flip ? <NewCard key={index} recipe={recipe} handleClick={handleClick} /> : <BackCardComponent key={index} recipe={recipe} handleClick={handleClick} />
+          )
+        })}
       </Container>
-
     </>
     
   );
---
+++ src/components/BackCard.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+import Timer from './Timer';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledBackCard = styled.div`
+  background-color: #181818;
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+  height: 400px;
+  border-radius: 5px;
+  border: 3px solid #b3b3b3;
+  box-shadow: 0px 0px 15px 5px #181818;
+
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+`;
+
+const StyledSubtitle = styled.h2``;
+
+const StyledQuote = styled.p`
+  color: #181818;
+  text-align: center;
+  background-color: #b3b3b3;
+  font-size: 10px;
+  padding: 10px 5px;
+  margin: 0;
+`;
+
+const StyledBrewInstructions = styled.ol`
+  color: #ffffff;
+`;
+
+const StyledBrewStep = styled.li`
+  color: #ffffff; 
+  font-size: 11px;
+  line-height: 1.5;
+`;
+
+const StyledButton = styled.button`
+  background-color:${({primary}) => primary ? "#181818" : "#b3b3b3"}; 
+  position: absolute;
+  color: #ffffff;
+  font-size: 15px;
+  bottom: 10px;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  margin-top: 15px;
+`;
+
+const BackCardComponent = ({ recipe, handleClick }) => { 
+  return (
+    <Container>
+        <StyledBackCard>
+        <StyledSubtitle>{recipe.name}</StyledSubtitle>
+        <StyledQuote>{recipe.quote}</StyledQuote>
+        <StyledBrewInstructions>
+            {Object.keys(recipe.instructions).map((step, index) => (
+            <StyledBrewStep key={index}>{recipe.instructions[step]}</StyledBrewStep>
+            ))}
+        </StyledBrewInstructions>
+        <StyledButton onClick={handleClick}>Back</StyledButton>
+        <Timer />
+        </StyledBackCard>
+    </Container>
+  );
+}
+
+export default BackCardComponent;
